d9a5506e8fc3df6ff697e820376101b5de5206e5
Thiyan <thiyaneng@gmail.com>
2020-03-01 10:13:57 +0530

    get one device

--- src/main/java/yanmakes/patient_iot/code/APIs.java
+++ src/main/java/yanmakes/patient_iot/code/APIs.java
@@ -3,7 +3,10 @@ package yanmakes.patient_iot.code;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import yanmakes.patient_iot.exception.CustException;
+import yanmakes.patient_iot.exception.ExStatus;
 import yanmakes.patient_iot.utils.CusResponse;
 
 @RestController
@@ -24,6 +27,18 @@ public class APIs {
     public CusResponse addDevice(){
 
         return new CusResponse(repo.save(new Device("001",90.8,false,false,"Kitchen","House")));
+
+    }
+
+    @GetMapping("/device")
+    public CusResponse oneDevice(@RequestParam String number) throws CustException {
+
+        Device device=repo.findByNumber(number);
+
+        if (device==null)
+            throw  new CustException(ExStatus.NO_ENTRY_FOUND);
+
+        return new CusResponse(device);
 
     }
 
--- src/main/java/yanmakes/patient_iot/exception/ExceptionController.java
+++ src/main/java/yanmakes/patient_iot/exception/ExceptionController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import yanmakes.patient_iot.utils.CusResponse;
 
 
 //@RestController
@@ -16,10 +17,10 @@ class ExceptionController {
 
     @ResponseBody
     @ExceptionHandler(value = CustException.class)
-    public CustException customExceptionHandler(CustException ex){
+    public CusResponse customExceptionHandler(CustException ex){
         LOGGER.error("In Exception handler");
-        ex.printStackTrace();
-        CustException response=new CustException(ex.getStatus());
+//        ex.printStackTrace();
+        CusResponse response=new CusResponse(ex.getStatus());
         return response;
     }
 
@@ -27,7 +28,7 @@ class ExceptionController {
     @ExceptionHandler(value = Exception.class)
     public CustException ExceptionHandler(Exception ex){
         System.out.println("In Exception handler");
-        ex.printStackTrace();
+//        ex.printStackTrace();
         CustException response=new CustException(ExStatus.ERROR);
         return response;
     }
